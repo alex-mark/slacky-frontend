43685d388bf0e922aabacb80129824c8fb001958
alex-mark <alexndr86@gmail.com>
2018-02-04 18:20:13 +0300

    errors on register screen

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import { Container, Header, Input, Button } from 'semantic-ui-react';
+import { Container, Header, Input, Button, Message } from 'semantic-ui-react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 class Register extends Component {
   state = {
     username: '',
+    usernameError: '',
     email: '',
+    emailError: '',
     password: '',
+    passwordError: '',
   };
 
   onChange = e => {
@@ -16,27 +19,66 @@ class Register extends Component {
   };
 
   onSubmit = async () => {
-    const response = await this.props.mutate({
-      variables: this.state,
+    this.setState({
+      usernameError: '',
+      emailError: '',
+      passwordError: '',
     });
+
+    const { username, email, password } = this.state;
+    const response = await this.props.mutate({
+      variables: { username, email, password },
+    });
+
+    const { ok, errors } = response.data.register;
+    if (ok) {
+      this.props.history.push('/');
+    } else {
+      const err = {};
+      errors.forEach(({ path, message }) => {
+        err[`${path}Error`] = message;
+      });
+      this.setState(err);
+    }
+
     console.log(response);
-  }
+  };
 
   render() {
-    const { username, email, password } = this.state;
+    const { username, email, password, usernameError, emailError, passwordError } = this.state;
 
+    const errorList = [];
+    if (usernameError) {
+      errorList.push(usernameError);
+    }
+    if (emailError) {
+      errorList.push(emailError);
+    }
+    if (passwordError) {
+      errorList.push(passwordError);
+    }
+    
     return (
       <Container text>
         <Header as="h2">Register Form</Header>
         <Input
+          error={!!usernameError}
           name="username"
           onChange={this.onChange}
           value={username}
           placeholder="Username"
           fluid
         />
-        <Input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid />
         <Input
+          error={!!emailError}
+          name="email"
+          onChange={this.onChange}
+          value={email}
+          placeholder="Email"
+          fluid
+        />
+        <Input
+          error={!!passwordError}
           name="password"
           onChange={this.onChange}
           value={password}
@@ -44,7 +86,14 @@ class Register extends Component {
           type="password"
           fluid
         />
-        <Button onClick={this.onSubmit} >Submit</Button>
+        <Button onClick={this.onSubmit}>Submit</Button>
+        {usernameError || emailError || passwordError ? (
+          <Message
+            error
+            header="There was some errors with your submission"
+            list={errorList}
+          />
+        ) : null}
       </Container>
     );
   }
@@ -52,7 +101,13 @@ class Register extends Component {
 
 const registerMutation = gql`
   mutation($username: String!, $email: String!, $password: String!) {
-    register(username: $username, email: $email, password: $password)
+    register(username: $username, email: $email, password: $password) {
+      ok
+      errors {
+        path
+        message
+      }
+    }
   }
 `;
 
