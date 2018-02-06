80609d47be065ad402191b9d20bea7ecd3814531
alex-mark <alexndr86@gmail.com>
2018-02-06 21:03:58 +0300

    createTeam Mutation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloLink, concat, from } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 import 'semantic-ui-css/semantic.min.css';
@@ -9,10 +11,47 @@ import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
 import Routes from './routes';
 
+const httpLink = createHttpLink({ uri: 'http://localhost:8080/graphql' });
+
+const authMiddleware = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token') || null;
+  const refreshToken = localStorage.getItem('refreshToken') || null;
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      'x-token': token,
+      'x-refresh-token': refreshToken,
+    },
+  };
+});
+
+const afterwareLink = new ApolloLink((operation, forward) => {
+  const { headers } = operation.getContext();
+
+  if (headers) {
+    const token = headers.get('x-token');
+    const refreshToken = headers.get('x-refresh-token');
+
+    if (token) {
+      localStorage.setItem('token', token);
+    }
+
+    if (refreshToken) {
+      localStorage.setItem('refreshToken', refreshToken);
+    }
+  }
+
+  return forward(operation);
+});
+
 const client = new ApolloClient({
-  // By default, this client will send queries to the
-  //  `/graphql` endpoint on the same host
-  link: new HttpLink({ uri: 'http://localhost:8080/graphql' }),
+  link: from([
+    afterwareLink,
+    authMiddleware,
+    httpLink,
+  ]),
   cache: new InMemoryCache(),
 });
 
