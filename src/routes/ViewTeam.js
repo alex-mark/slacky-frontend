8b8306b85ac52c3fd0d1970acf5a066d542a2d7a
alex-mark <alexndr86@gmail.com>
2018-02-08 01:45:47 +0300

    allTeams query

--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -1,21 +1,15 @@
 import React from 'react';
 
 import AppLayout from '../components/AppLayout';
-import Teams from '../components/Teams';
-import Channels from '../components/Channels';
+
 import Header from '../components/Header';
 import Messages from '../components/Messages';
 import SendMessage from '../components/SendMessage';
+import Sidebar from '../containers/Sidebar';
 
 export default () => (
   <AppLayout>
-    <Teams teams={[{ id: 1, letter: 'B' }, { id: 2, letter: 'Q' }]} />
-    <Channels
-      teamName="Team name"
-      username="Username"
-      channels={[{ id: 1, name: 'general' }, { id: 2, name: 'random' }]}
-      users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'user1' }]}
-    />
+    <Sidebar currentTeamId={21} />
     <Header channelName="general" />
     <Messages />
     <SendMessage channelName="general" />
